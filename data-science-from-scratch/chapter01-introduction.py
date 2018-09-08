92390674475233226e33b93d341f38b0352f9cb8
Fernanda Gomes <fergomes@gmail.com>
2018-09-08 19:56:07 +0200

    finish Chater 1

--- data-science-from-scratch/chapter01-introduction.py
+++ data-science-from-scratch/chapter01-introduction.py
@@ -128,3 +128,65 @@ def most_common_interests_with(user):
 #    SALARIES AND EXPERIENCE   #
 #                              #
 ################################
+
+salaries_and_tenures = [(83000, 8.7), (88000, 8.1),
+                        (48000, 0.7), (76000, 6),
+                        (69000, 6.5), (76000, 7.5),
+                        (60000, 2.5), (83000, 10),
+                        (48000, 1.9), (63000, 4.2)]
+
+def tenure_bucket(tenure):
+    if tenure < 2:
+        return "less than 2"
+    elif tenure < 5:
+        return "between 2 and 5"
+    else:
+        return "more than 5"
+
+# keys are tenure buckets, values are lists of salaries for that bucket
+salary_by_tenure_bucket = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+    bucket = tenure_bucket(tenure)
+    salary_by_tenure_bucket[bucket].append(salary)
+
+# compute the avarage salary for each bucket
+
+# keys are tenure buckets, values are average salary for that bucket
+average_salary_by_bucket = {
+    tenure_bucket : sum(salaries) / len(salaries)
+    for tenure_bucket, salaries in salary_by_tenure_bucket.items()
+}
+
+print(average_salary_by_bucket)
+
+
+################################
+#                              #
+#         PAID ACCOUNT         #
+#                              #
+################################
+
+def predict_paid_or_unpaid(years_experience):
+    if years_experience < 3.0:
+        return "paid"
+    elif years_experience < 8.5:
+        return "unpaid"
+    else:
+        return "paid"
+
+
+
+################################
+#                              #
+#      TOPICS OF INTEREST      #
+#                              #
+################################
+
+words_and_counts = Counter (word
+                            for user, interest in interests
+                            for word in interest.lower().split())
+
+for word, count in words_and_counts.most_common():
+    if count > 1:
+        print(word, count)
