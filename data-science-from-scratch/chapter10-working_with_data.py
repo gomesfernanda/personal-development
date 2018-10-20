7232cb759ee5956565ecd66a64461b18c35514a5
Fernanda Gomes <fergomes@gmail.com>
2018-10-20 21:40:39 +0200

    add DIMENSIONALITY REDUCTION completing chapter10

--- data-science-from-scratch/chapter10-working_with_data.py
+++ data-science-from-scratch/chapter10-working_with_data.py
@@ -2,9 +2,11 @@ import math
 import matplotlib.pyplot as plt
 from collections import Counter, defaultdict
 import random
-from chapter04_linearalgebra import shape, get_column, make_matrix
+from functools import partial
+from chapter04_linearalgebra import shape, get_column, make_matrix, magnitude, dot, vector_sum, scalar_multiply, vector_subtract
 from chapter05_statistics import correlation, mean, standard_deviation
 from chapter06_probability import inverse_normal_cdf
+from chapter08_gradientdescent import maximize_batch, maximize_stochastic
 
 ###########################
 #                         #
@@ -223,3 +225,86 @@ def rescale(data_matrix):
 
     num_rows, num_cols = shape(data_matrix)
     return make_matrix(num_rows, num_cols, rescaled)
+
+
+############################
+#                          #
+# DIMENSIONALITY REDUCTION #
+#                          #
+############################
+
+
+def de_mean_matrix(A):
+    """return the result of subtracting from every value in A the mean
+    value of its column. the resulting matrix has mean 0 in every column"""
+    nr, nc = shape(A)
+    column_means, _ = scale(A)
+    return make_matrix(nr, nc, lambda i, j: A[i][j] - column_means[j])
+
+def direction(w):
+    mag = magnitude(w)
+    return [w_i / mag for w_i in w]
+
+def directional_variance_i(x_i, w):
+    """the variance of the row x_i in the direction determined by w"""
+    return dot(x_i, direction(w))
+
+def directional_variance(X, w):
+    """the variance for the data in the direction determined by w"""
+    return sum(directional_variance_i(x_i, w) for x_i in X)
+
+def directional_variance_gradient_i(x_i, w):
+    """the contribution of row x_i to the gradient of
+    the direction-w variance"""
+    projection_length = dot(x_i, direction(w))
+    return [2 * projection_length * x_ij for x_ij in x_i]
+
+def directional_variance_gradient(X, w):
+    return vector_sum(directional_variance_gradient_i(x_i, w) for x_i in X)
+
+def first_principal_component(X):
+    guess = [1 for _ in X[0]]
+    unscaled_maximizer = maximize_batch(
+        partial(directional_variance, X),
+        partial(directional_variance_gradient, X),
+        guess)
+    return direction(unscaled_maximizer)
+
+def first_principal_component_sgd(X):
+    guess = [1 for _ in X[0]]
+    unscaled_maximizer = maximize_stochastic(
+        lambda x, _, w: directional_variance_i(x, w),
+        lambda x, _, w: directional_variance_gradient_i(x, w),
+        X,
+        [None for _ in X],     # the "fake "y"
+        guess)
+    return direction(unscaled_maximizer)
+
+def project(v, w):
+    """return the projection of v onto the direction of w"""
+    projection_length = dot(v, w)
+    return scalar_multiply(projection_length, w)
+
+def remove_projection_from_vector(v, w):
+    """projects v onto w and subtracts the result from v"""
+    return vector_subtract(v, project(v, w))
+
+def remove_projection(X, w):
+    """for each row of X,
+    projects the row onto w, and subtracts the result from the row"""
+    return [remove_projection_from_vector(x_i, w) for x_i in X]
+
+def principal_component_analysis(X, num_components):
+    components = []
+    for _ in range(num_components):
+        component = first_principal_component(X)
+        components.append(component)
+        X = remove_projection(X, component)
+
+    return components
+
+def transform_vector(v, components):
+    return [dot(v, w) for w in components]
+
+def transform(X, components):
+    return[transform_vector(x_i, components) for x_i in X]
