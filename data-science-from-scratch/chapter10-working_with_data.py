49e5e4df12e573f0998b8030a675c80b34af85f4
Fernanda Gomes <fergomes@gmail.com>
2018-10-20 19:56:19 +0200

    add CLEANING AND MUNGING section

--- data-science-from-scratch/chapter10-working_with_data.py
+++ data-science-from-scratch/chapter10-working_with_data.py
@@ -2,6 +2,8 @@ import math
 import matplotlib.pyplot as plt
 from collections import Counter
 import random
+from chapter04_linearalgebra import shape, get_column, make_matrix
+from chapter05_statistics import correlation
 from chapter06_probability import inverse_normal_cdf
 
 ###########################
@@ -39,14 +41,108 @@ def random_normal():
     """returns a random draw from a standard normal distribution"""
     return inverse_normal_cdf(random.random())
 
-xs = [random_normal() for _ in range(1000)]
-ys1 = [ x + random_normal() / 2 for x in xs]
-ys2 = [ -x + random_normal() / 2 for x in xs]
+def plot_different_joint_distributinos():
+    xs = [random_normal() for _ in range(1000)]
+    ys1 = [ x + random_normal() / 2 for x in xs]
+    ys2 = [ -x + random_normal() / 2 for x in xs]
 
-plt.scatter(xs, ys1, marker='.', color='black', label="ys1")
-plt.scatter(xs, ys2, marker='.', color='gray', label="ys2")
-plt.xlabel("xs")
-plt.ylabel("ys")
-plt.legend(loc=9)
-plt.title("Very different joint distributions")
-plt.show()
+    plt.scatter(xs, ys1, marker='.', color='black', label="ys1")
+    plt.scatter(xs, ys2, marker='.', color='gray', label="ys2")
+    plt.xlabel("xs")
+    plt.ylabel("ys")
+    plt.legend(loc=9)
+    plt.title("Very different joint distributions")
+    plt.show()
+
+def correlation_matrix(data):
+    """returns the num_columns vs num_columns matrix whose (i, j)th entry
+    is the correlation between columns i and j of data"""
+
+    _, num_columns = shape(data)
+
+    def matrix_entry(i, j):
+        return correlation(get_column(data, i), get_column(data, j))
+
+    return make_matrix(num_columns, num_columns, matrix_entry)
+
+def plot_scatterplot_matrix():
+    # first, generate some random data
+
+    num_points = 100
+
+    def random_row():
+        row = [None, None, None, None]
+        row[0] = random_normal()
+        row[1] = -5 * row[0] + random_normal()
+        row[2] = row[0] + row[1] + 5 * random_normal()
+        row[3] = 6 if row[2] > -2 else 0
+        return row
+    random.seed(0)
+    data = [random_row()
+            for _ in range(num_points)]
+
+    _, num_columns = shape(data)
+
+    fig, ax = plt.subplots(num_columns, num_columns)
+
+    for i in range(num_columns):
+        for j in range(num_columns):
+
+            # scatter column_j on the x-axis vs column_i on the y-axis
+            if i != j: ax[i][j].scatter(get_column(data, j), get_column(data, i))
+
+            # unless i == j, in which case show the series name
+            else: ax[i][j].annotate("series " + str(i), (0.5, 0.5),
+                                    xycoords='axes fraction', ha="center", va="center")
+
+            # then hide axis labels except left and bottom charts
+            if i < num_columns - 1: ax[i][j].xaxis.set_visible(False)
+            if j > 0: ax[i][j].yaxis.set_visible(False)
+
+    # fix the bottom right and top left axis labels, which are wrong because
+    # their charts only have text in them
+    ax[-1][-1].set_xlim(ax[0][-1].get_xlim())
+    ax[0][0].set_ylim(ax[0][1].get_ylim())
+
+    plt.show()
+
+# plot_scatterplot_matrix()
+
+
+###########################
+#                         #
+#  CLEANING AND MUNGING   #
+#                         #
+###########################
+
+
+def try_or_none(f):
+    """wraps f to return None if f raises an exception
+    assumes f takes only one input"""
+    def f_or_none(x):
+        try: return f(x)
+        except: return None
+    return f_or_none
+
+def parse_row(input_row, parsers):
+    """given a list of parsers (some of which may be None)
+    apply the appropriate one to each element of the input_row"""
+    return [try_or_none(parser)(value) if parser is not None else value
+            for value, parser in zip(input_row, parsers)]
+
+def parser_rows_with(reader, parsers):
+    """wrap a reader to apply the parsers to each of its rows"""
+    for row in reader:
+        yield parse_row(row, parsers)
+
+def try_parse_field(field_name, value, parser_dict):
+    """try to parse value using the appropriate function from parser_dict"""
+    parser = parser_dict.get(field_name)        # None if no such entry
+    if parser is not None:
+        return try_or_none(parser)(value)
+    else:
+        return value
+
+def parse_dict(input_dict, parser_dict):
+    return { field_name : try_parse_field(field_name, value, parser_dict)
+             for field_name, value in input_dict.items() }
