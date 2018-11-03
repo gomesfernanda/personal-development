de8ad208a487bd99d3ab7bdd695c0c374f09d9fb
Fernanda Gomes <fergomes@gmail.com>
2018-11-03 16:17:36 +0100

    renaming files

--- data-science-from-scratch/chapter11_machine_learning.py
+++ data-science-from-scratch/chapter11_machine_learning.py
@@ -1,10 +1,10 @@
 import random
 
 def split_data(data, prob):
-    """split data into fractions [prob, 1 - prob] where prob is the fraction that will be saved for training"""
+    """split data into fractions [prob, 1 - prob]"""
     results = [], []
     for row in data:
-        results[0 if random.random() < prob else 1].append[row]
+        results[0 if random.random() < prob else 1].append(row)
     return results
 
 def train_test_split(x, y, test_pct):
