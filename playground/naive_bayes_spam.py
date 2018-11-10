3cfdb4825931971b24a7a74d02422226934b312c
Fernanda Gomes <fergomes@gmail.com>
2018-11-10 19:02:54 +0100

    add Bernoulli Naive Bayes and compare to Multinomial NB

--- playground/naive_bayes_spam.py
+++ playground/naive_bayes_spam.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score, recall_score, accuracy_score
 
 
+####### On this file, we used the SMS Spam dataset available on: https://data.world/lylepratt/sms-spam
+
 def get_data_sms(filename):
     data_sms = []
     with open(filename, 'r', encoding='utf-8') as file:
@@ -18,8 +20,10 @@ def get_data_sms(filename):
 
 file = "SMSSpamCollection.txt"
 data_sms = get_data_sms(file)
+
 target = []
 features = []
+
 for i in range(len(data_sms)):
     features.append(data_sms[i][0])
     target.append(data_sms[i][1])
@@ -29,14 +33,32 @@ X = vectorizer.fit_transform(features)
 
 X_train, X_test, y_train, y_test = train_test_split(X, target, test_size=.4, random_state=42)
 
+print("Using Multinomial Naive Bayes\n")
+
 mnb = MultinomialNB()
 
-y_pred = mnb.fit(X_train, y_train).predict(X_test)
+y_pred_mnb = mnb.fit(X_train, y_train).predict(X_test)
 
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
+accuracy_mnb = round(accuracy_score(y_test, y_pred_mnb), 4)
+precision_mnb = round(precision_score(y_test, y_pred_mnb), 4)
+recall_mnb = round(recall_score(y_test, y_pred_mnb), 4)
 
-print("Accuracy:", accuracy)
-print("Precision:", precision)
-print("Recall:", recall)
+print("Accuracy:", accuracy_mnb)
+print("Precision:", precision_mnb)
+print("Recall:", recall_mnb)
+
+print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
+print("Using Bernoulli Naive Bayes\n")
+
+bnb = BernoulliNB()
+
+y_pred_bnb = bnb.fit(X_train, y_train).predict(X_test)
+
+accuracy_bnb = round(accuracy_score(y_test, y_pred_bnb), 4)
+precision_bnb = round(precision_score(y_test, y_pred_bnb), 4)
+recall_bnb = round(recall_score(y_test, y_pred_bnb), 4)
+
+print("Accuracy:", accuracy_bnb)
+print("Precision:", precision_bnb)
+print("Recall:", recall_bnb)
