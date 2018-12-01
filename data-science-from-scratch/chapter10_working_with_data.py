58dfc41ebd0b5842db792dca64d11515c09d2451
Fernanda Gomes <fergomes@gmail.com>
2018-12-01 21:02:55 +0100

    I did something wrong

--- data-science-from-scratch/chapter10_working_with_data.py
+++ data-science-from-scratch/chapter10_working_with_data.py
@@ -203,23 +203,22 @@ def day_over_day_changes(grouped_rows):
 ###########################
 
 def scale(data_matrix):
-    """returns the means and standard deviations of each column"""
-    num_rows, num_columns = shape(data_matrix)
-    means = [mean(get_column(data_matrix, j))
-             for j in range(num_columns)]
-    stdevs = [standard_deviation(get_column(data_matrix, j))
-              for j in range(num_columns)]
+    num_rows, num_cols = shape(data_matrix)
+    means = [mean(get_column(data_matrix,j))
+             for j in range(num_cols)]
+    stdevs = [standard_deviation(get_column(data_matrix,j))
+              for j in range(num_cols)]
     return means, stdevs
 
 def rescale(data_matrix):
     """rescales the input data so that each column
     has mean 0 and standard deviation 1
-    leaves alone columns with no deviation"""
+    ignores columns with no deviation"""
     means, stdevs = scale(data_matrix)
 
     def rescaled(i, j):
         if stdevs[j] > 0:
-            return(data_matrix[i][j] - means[j]) / stdevs[j]
+            return (data_matrix[i][j] - means[j]) / stdevs[j]
         else:
             return data_matrix[i][j]
 
