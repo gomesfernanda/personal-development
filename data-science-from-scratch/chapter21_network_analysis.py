5684b045a625dd482f38dd852b9e83ffc676429e
Fernanda Gomes <fergomes@gmail.com>
2019-04-01 00:00:16 +0200

    Add comments that were in the book

--- data-science-from-scratch/chapter21_network_analysis.py
+++ data-science-from-scratch/chapter21_network_analysis.py
@@ -20,8 +20,9 @@ for user in users:
     user["friends"] = []
 
 for i, j in friendships:
-    users[i]["friends"].append(users[j])
-    users[j]["friends"].append(users[i])
+    # this works because users[i] is the user whose id is i
+    users[i]["friends"].append(users[j])        # add i as a friend of j
+    users[j]["friends"].append(users[i])        # add j as a friend of i
 
 
 ##############################
